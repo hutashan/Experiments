a62bfd99a291b3752661ac72846709f462288f9b
narendrasfo <narendra.sfo@gmail.com>
2017-02-03 16:18:39 -0800

    added new

---
+++ src/main/java/simpleprob/Gcd.java
@@ -0,0 +1,37 @@
+package simpleprob;
+
+import java.util.Random;
+
+/**
+ * Created by parmana on 2/2/17.
+ */
+public class Gcd {
+
+    public static void main(String[] args) {
+
+       // System.out.println(new Random().nextInt(5));
+       Random rn= new Random();
+        rn.setSeed(10);
+        System.out.println(rn.nextLong());
+        System.out.println(gcd(10,12));
+        System.out.println(gcd2(10,12));
+    }
+
+
+    public static int gcd(int p,int q){
+        if(q==0) return p;
+        else
+          return   gcd(q,p%q);
+    }
+
+    public static int gcd2(int p,int q){
+      while (q!=0){
+          int temp=q;
+          q=p%q;
+          p=temp;
+      }
+        return p;
+    }
+
+
+}
