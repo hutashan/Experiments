108fb82dbc70500fa1f3688e5ac5bcc42f75c5dd
Narendra <narendra.sfo@gmail.com>
2017-08-01 14:49:50 -0700

    tries

--- src/main/java/datastructure/TriesNode.java
+++ src/main/java/datastructure/TriesNode.java
@@ -4,7 +4,8 @@ import java.util.HashMap;
 
 
 public class TriesNode {
-    HashMap<Character,TriesNode> dict = new  HashMap<Character,TriesNode>();
-    char c;
-    boolean isleaf;
+    public HashMap<Character,TriesNode> dict = new  HashMap<Character,TriesNode>();
+    public char c;
+    public  boolean isleaf=false;
+    public String key;
 }
